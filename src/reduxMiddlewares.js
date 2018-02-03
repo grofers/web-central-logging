8b4b1441b103f352f2c2baae3a8af4a16493a19c
priyansh gupta <priyanshgupta1161@gmail.com>
2018-02-03 13:32:08 +0530

    Logster class seperated from anything related to redux

--- src/reduxMiddlewares.js
+++ src/reduxMiddlewares.js
@@ -1,13 +1,32 @@
-const actionLogger = (logger, level = 'info') => store => next => (action) => {
-    logger.report(level, action, store.getState());
-    return next(action);
+const actionLogger = (
+    logger,
+    actionFilter = f => f,
+    stateFilter = () => null,
+    level = 'info',
+) => store => next => (action) => {
+    const filteredAction = actionFilter(action);
+    const filteredStateBA = stateFilter(store.getState());
+
+    next(action);
+
+    const filteredStateAA = stateFilter(store.getState());
+    const state = (filteredStateBA || filteredStateAA) ? {
+        before: filteredStateBA,
+        after: filteredStateAA,
+    } : {};
+
+    logger.report(level, filteredAction, state);
 };
 
-const crashReporter = (logger, level = 'error') => store => next => (action) => {
+const crashReporter = (
+    logger,
+    stateFilter = () => null,
+    level = 'error'
+) => store => next => (action) => {
     try {
         return next(action);
     } catch (e) {
-        logger.report(level, e, store.getState());
+        logger.report(level, e, stateFilter(store.getState()));
         throw e;
     }
 };
